229a8f89546eb16ed0f2793214cbc0ef36985deb
김민지 <minjee@deali.net>
2021-02-19 15:18:14 +0900

    AmazingPriceView 스토리보드에서 사용가능하도록 수정 및 샘플코드 작성

--- AmazingPriceView/Classes/AmazingPriceView.swift
+++ AmazingPriceView/Classes/AmazingPriceView.swift
@@ -104,15 +104,13 @@ open class AmazingPriceView: UIView {
         didSet {
             if oldValue == self.isOverMaximum { return }
             
-            guard let delegate = self.delegate else { return }
-
-            delegate.isPriceOverMaximumPrice(isOverMaximumPrice: self.isOverMaximum)
-
             if self.isOverMaximum {
                 setMaximumPriceAnimation()
             } else {
                 restoreNumberViews()
             }
+            
+            self.delegate?.isPriceOverMaximumPrice(isOverMaximumPrice: self.isOverMaximum)
         }
     }
 
@@ -126,22 +124,35 @@ open class AmazingPriceView: UIView {
         }
     }
     
-    public init() {
+    @IBInspectable var numberFont: UIFont = .systemFont(ofSize: 36, weight: .bold)
+    @IBInspectable var maxPrice: Int = 2000000
+    @IBInspectable var minPrice: Int = 1000
+    
+//    public init() {
+//        super.init(frame: .zero)
+//    }
+    
+    public init(font: UIFont = .systemFont(ofSize: 36.0), minimumPrice: Int = 1000, maximumPrice: Int = 2000000) {
         super.init(frame: .zero)
+        
+        self.initLayout(font: font, minPrice: minimumPrice, maxPrice: maximumPrice)
     }
     
-    public init(font: UIFont = .systemFont(ofSize: 36.0), fontSize: CGFloat = 36.0, minimumPrice: Int = 1000, maximumPrice: Int = 2000000) {
-        super.init(frame: .zero)
-        
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        self.initLayout(font: self.numberFont, minPrice: self.minPrice, maxPrice: self.maxPrice)
+    }
+    
+    private func initLayout(font: UIFont, minPrice: Int, maxPrice: Int) {
         self.font = font
-        self.font.withSize(fontSize)
-        
-        self.minimumPrice = minimumPrice
-        self.maximumPrice = maximumPrice
+        self.minimumPrice = minPrice
+        self.maximumPrice = maxPrice
         
         initNumberViews()
         initPlaceHolder()
     }
+    
     
     private func initPlaceHolder() {
         
@@ -156,8 +167,8 @@ open class AmazingPriceView: UIView {
         self.placeHolderView.widthAnchor.constraint(equalToConstant: self.placeHolderView.intrinsicContentSize.width + 0.5).isActive = true
         self.placeHolderView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         self.placeHolderView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        self.placeHolderView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        self.placeHolderView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+//        self.placeHolderView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+//        self.placeHolderView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         self.placeHolderView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
     }
     
@@ -272,6 +283,17 @@ open class AmazingPriceView: UIView {
         })
     }
     
+    public func deleteNumber() {
+        
+        if self.price == 0 { return }
+        
+        self.price = self.price / 10
+        
+        self.isOverMaximum = false
+        
+        self.deleteNumberAnimation()
+    }
+    
     private func deleteNumberAnimation() {
         
         self.removeFloatingPoint()
@@ -306,17 +328,6 @@ open class AmazingPriceView: UIView {
             
             numberView.transform = CGAffineTransform(translationX: 0.0, y: 0.0)
         })
-    }
-    
-    public func deleteNumber() {
-        
-        if self.price == 0 { return }
-        
-        self.price = self.price / 10
-        
-        self.isOverMaximum = false
-        
-        self.deleteNumberAnimation()
     }
     
     private func setMaximumPriceAnimation() {
@@ -445,9 +456,5 @@ open class AmazingPriceView: UIView {
             self.currency = "$"
             self.floatingPoint = ","
         }
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
--- Example/AmazingPriceView/ViewController.swift
+++ Example/AmazingPriceView/ViewController.swift
@@ -9,96 +9,55 @@
 import UIKit
 import AmazingPriceView
 
+
 @available(iOS 10.0, *)
 class ViewController: UIViewController {
-
-    let insertButton = UIButton()
-    let insertButton2 = UIButton()
-    let insertButton3 = UIButton()
-
-    let clearButton = UIButton()
-    let deleteButton = UIButton()
-    var testView: AmazingPriceView?
+    
+    private let HORIZONTAL_MARGIN: CGFloat = 32.0
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceView: AmazingPriceView!
+    @IBOutlet weak var keypadCollectionView: UICollectionView!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    /*
+    // INITIALIZE AMAZING PRICE VIEW IN CODE
+    var priceView = AmazingPriceView(font: .boldSystemFont(ofSize: 36.0), minimumPrice: 1000, maximumPrice: 2000000)
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.view.addSubview(self.priceView)
+        
+        self.priceView.translatesAutoresizingMaskIntoConstraints = false
+        self.priceView.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor, constant: 200.0).isActive = true
+        self.priceView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.priceView.backgroundColor = .systemYellow
+    } */
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.priceView.delegate = self
+        self.priceView.placeHolderColor = .lightGray
+        self.priceView.placeHolder = "금액"
+        self.priceView.placeHolderFont = .systemFont(ofSize: 26, weight: .medium)
+        
+        self.priceView.fontColor = .black
+        self.priceView.maximumPricefontColor = .systemPink
+    }
+    
+    func layoutPriceView() {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        testView = AmazingPriceView.init(.boldSystemFont(ofSize: 36.0), .black, .systemPink, 36.0, 2000000, 1000, 0.08, 0.10, 0.30, .KOR)
-//        testView?.initPlaceHolder("금액을 입력하세요", .boldSystemFont(ofSize: 36.0), .systemPink)
-        testView = AmazingPriceView(font: .boldSystemFont(ofSize: 36.0), fontSize: 36.0, minimumPrice: 1000, maximumPrice: 20000000000)
-        testView?.fontColor = .black
-        testView?.maximumPricefontColor = .blue
-        testView?.placeHolder = "안녕하세요"
-        testView?.delegate = self
-        self.view.addSubview(testView!)
-        testView?.translatesAutoresizingMaskIntoConstraints = false
-        testView?.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor, constant: 50.0).isActive = true
-        testView?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        
-        self.view.addSubview(self.insertButton)
-        self.insertButton.setTitle("1", for: .normal)
-        self.insertButton.backgroundColor = .lightGray
-        self.insertButton.translatesAutoresizingMaskIntoConstraints = false
-        self.insertButton.topAnchor.constraint(equalTo: self.testView!.bottomAnchor, constant: 100.0).isActive = true
-        self.insertButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.insertButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        self.insertButton.addTarget(self, action: #selector(insertOneClicked), for: .touchUpInside)
-        
-        self.view.addSubview(self.insertButton2)
-        self.insertButton2.setTitle("2", for: .normal)
-        self.insertButton2.backgroundColor = .lightGray
-        self.insertButton2.translatesAutoresizingMaskIntoConstraints = false
-        self.insertButton2.topAnchor.constraint(equalTo: self.insertButton.bottomAnchor, constant: 15.0).isActive = true
-        self.insertButton2.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.insertButton2.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        self.insertButton2.addTarget(self, action: #selector(insertTwoClicked), for: .touchUpInside)
-
-        self.view.addSubview(self.insertButton3)
-        self.insertButton3.setTitle("3", for: .normal)
-        self.insertButton3.backgroundColor = .lightGray
-        self.insertButton3.translatesAutoresizingMaskIntoConstraints = false
-        self.insertButton3.topAnchor.constraint(equalTo: self.insertButton2.bottomAnchor, constant: 15.0).isActive = true
-        self.insertButton3.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.insertButton3.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        self.insertButton3.addTarget(self, action: #selector(insertThreeClicked), for: .touchUpInside)
-        
-        self.view.addSubview(self.clearButton)
-        self.clearButton.setTitle("clear", for: .normal)
-        self.clearButton.backgroundColor = .lightGray
-        self.clearButton.translatesAutoresizingMaskIntoConstraints = false
-        self.clearButton.topAnchor.constraint(equalTo: self.insertButton3.bottomAnchor, constant: 15.0).isActive = true
-        self.clearButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.clearButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        self.clearButton.addTarget(self, action: #selector(clearClicked), for: .touchUpInside)
-
-        self.view.addSubview(self.deleteButton)
-        self.deleteButton.setTitle("delete", for: .normal)
-        self.deleteButton.backgroundColor = .lightGray
-        self.deleteButton.translatesAutoresizingMaskIntoConstraints = false
-        self.deleteButton.topAnchor.constraint(equalTo: self.clearButton.bottomAnchor, constant: 15.0).isActive = true
-        self.deleteButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.deleteButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        self.deleteButton.addTarget(self, action: #selector(deleteClicked), for: .touchUpInside)
-    }
-
-    @objc func clearClicked() {
-        self.testView?.clear()
-    }
-
-    @objc func deleteClicked() {
-        self.testView?.deleteNumber()
-    }
-
-    @objc func insertOneClicked() {
-        self.testView?.insertNumber(1)
-    }
-    
-    @objc func insertTwoClicked() {
-        self.testView?.insertNumber(2)
-    }
-    
-    @objc func insertThreeClicked() {
-        self.testView?.insertNumber(3)
     }
 
     override func didReceiveMemoryWarning() {
@@ -106,12 +65,75 @@ class ViewController: UIViewController {
     }
 }
 
-@available(iOS 10.0, *)
 extension ViewController: AmazingPriceViewDelegate {
     func isPriceOverMaximumPrice(isOverMaximumPrice: Bool) {
+        
+        if isOverMaximumPrice {
+            self.infoLabel.text = "Greater than MAX!"
+        } else {
+            self.infoLabel.text = "Less than MAX"
+        }
+        print("isPriceOverMaximumPrice: \(isOverMaximumPrice)")
     }
     
     func isPriceOverMinimumPrice(isOverMinimumPrice: Bool) {
+        if isOverMinimumPrice {
+            self.infoLabel.text = "Greater than MIN"
+        } else {
+            self.infoLabel.text = "Less than MIN!"
+        }
+        print("isPriceOverMinimumPrice: \(isOverMinimumPrice)")
     }
 }
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? KeypadCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.key = KeypadCell.getKey(indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let key = KeypadCell.getKey(indexPath)
+        
+        
+        switch KeypadCell.keyType(for: key) {
+        case .number:
+            self.priceView.insertNumber(key)
+            
+        case .delete:
+            self.priceView.deleteNumber()
+            
+        case .clear:
+            self.priceView.clear()
+            
+        default:
+            break
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (UIScreen.main.bounds.width-self.HORIZONTAL_MARGIN)/3, height: KeypadCell.HEIGHT)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
 
